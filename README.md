c0e80b7365152af917015d754258027b1163ec91
Jame Yu <yrh79@163.com>
2017-12-06 13:01:47 +0800

    initial version of the project

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# count_down_switch_serial_config
+# count_down_switch
+A count down switch implemented with Arduino Uno
+
+### Prerequisites
+* This project depends on the Timer library from https://github.com/JChristensen/Timer, please clone and place it into the libraries folder of your arduino IDE.
+
+## Description
+This is an implement of the count down switch with Arduino Uno.
+
+This project is an extended version of the https://github.com/yrh79/count_down_switch project, which has the capability of being configured through serial port. Another project that configures the device through a serial line would be implemented later with wxPython and the url to that would be included here later on.
+
+People may have the needs to restart a WIFI AP everyday just to make sure it works with no problem, but maybe that AP does not have the auto-reboot functionalities. Power cycle the AP may be a good way to keep it works perperly.
+
+This count down switch could switch off then back on the power suply of the power consumer (WIFI AP) on a hardware pre-configured basic. A user could use a set of jumppers (GPIO 4 through 9) to quickly setup a cycle value:
+
+
+### Quick setup:
+* pin 4 - 1 hour,
+* pin 5 - 2 hours,
+* pin 6 - 4 hours,
+* pin 7 - 6 hours,
+* pin 8 - 12 hours,
+* pin 9 - 48 hours,
+
+### Default setup:
+All-open - 24 hours (default value)
+
+### Bit-setup:
+1 bit equals to 30 minutes, e.g.:
+* 0x11 = 1.5 hours (jumpper set on pin 4 and 5)
+* 0x101 - 2.5 hours (jumpper set on pin 4 and 6)
+
+### Initial switching delay:
+The switch would do a switch off-on action 11 hours after the initial power on - this is to allow people setup the power supply (e.g.) on 2pm in the afternoon, then the first (and every power cycle) action would happen at 1am in the mid-night.
+
+### Serial Configuration
+* To configure the initial delay value, use the below command:
+```
+$init {0, 11, 0, 0}
+This would set the initial delay value to 11 hours.
+```
+
+* To configure the interval value, use the below command:
+```
+$cycle {0, 1, 0, 0}
+This would set the interval value to 1 hour.
+```
+
+### Basic Usage
+
+* Connect Pin 2 to a relay's input and power it properly.
+* Clone https://github.com/JChristensen/Timer and place it into the libraries folder of your arduino IDE.
+* Flash the Arduino Uno as usual then enjoy. :)
+
+This project is tested under arduno 1.8.5. Other version of arduino IDE may also work.
+
---
+++ count_down_switch_serial_config.ino
@@ -0,0 +1,335 @@
+#include "Timer.h"
+#include <EEPROM.h>
+
+Timer t;
+
+int ledEvent;
+
+typedef struct {
+  int day;
+  int hour;
+  int minute;
+  int second;
+} time_t;
+
+time_t countdown = {0, 0, 0, 0};
+
+//GPIO 2 is powerPin
+const int powerPin = 2;
+
+//GPIO 13 is LED pin
+const int ledPin = 13;
+
+//GPIO pin for configuration
+const int num_config_bits = 6;
+const int bit1_pin = 4;
+const int bit2_pin = 5;
+const int bit3_pin = 6;
+const int bit4_pin = 7;
+const int bit5_pin = 8;
+const int bit6_pin = 9;
+
+int configuration = 0;
+
+//power switch works for 5 seconds
+const int switch_seconds = 5;
+
+//Serial config stuffs
+int recording = 0;
+int idx = 0;
+const int bufSize = 128;
+char serialBuf[bufSize] = {0};
+
+//EEPROM
+#define EEPROM_SIZE         1024 //EEPROM size of arduino nano is 1k
+#define CONFIG_ADDR         0
+#define CONFIG_LEN          sizeof(time_t)
+#define INIT_ADDR           CONFIG_ADDR+CONFIG_LEN
+#define INIT_LEN          sizeof(time_t)
+
+typedef enum {
+  INIT = 0,
+  CYCLE = 1
+} config_type_t;
+
+void setup()
+{
+  //initialize countdown to 11 hours at the first boot
+  countdown = getConfig(INIT);
+
+  Serial.begin(115200);
+
+  //switch on powerPin to indicate the device is working
+  //then switch off to indicate the devides is in action
+  //start up event would switch on again in 5 seconds
+  pinMode(powerPin, OUTPUT);
+
+  switchOn();
+  delay(1000);
+  switchOff();
+
+  int tickEvent = t.every(1000, doCountDown);
+
+  pinMode(ledPin, OUTPUT);
+  ledEvent = t.oscillate(ledPin, 50, HIGH);
+
+  int afterEvent = t.after(switch_seconds * 1000, slowDownLed_switchOn);
+
+  //set pull down resisters for configuration pins
+  pinMode(bit1_pin, OUTPUT);
+  pinMode(bit2_pin, OUTPUT);
+  pinMode(bit3_pin, OUTPUT);
+  pinMode(bit4_pin, OUTPUT);
+  pinMode(bit5_pin, OUTPUT);
+  pinMode(bit6_pin, OUTPUT);
+
+  digitalWrite(bit1_pin, LOW);
+  digitalWrite(bit2_pin, LOW);
+  digitalWrite(bit3_pin, LOW);
+  digitalWrite(bit4_pin, LOW);
+  digitalWrite(bit5_pin, LOW);
+  digitalWrite(bit6_pin, LOW);
+
+  pinMode(bit1_pin, INPUT);
+  pinMode(bit2_pin, INPUT);
+  pinMode(bit3_pin, INPUT);
+  pinMode(bit4_pin, INPUT);
+  pinMode(bit5_pin, INPUT);
+  pinMode(bit6_pin, INPUT);
+
+}
+
+void loop()
+{
+  t.update();
+
+  while (Serial.available()) {
+    char c = Serial.read();
+
+    switch (c) {
+      case '\n':
+        break;
+      case '\r':
+        parseSerialConfig();
+        memset(serialBuf, 0, bufSize);
+        idx = 0;
+        break;
+      case '$':
+        recording = 1;
+        break;
+      default:
+        if (recording && (idx < bufSize)) {
+          serialBuf[idx] = c;
+          idx ++;
+        }
+        break;
+    }
+  }
+}
+
+void parseSerialConfig()
+{
+  time_t *c, configData = {0};
+  char msg[64] = {0};
+  c = &configData;
+
+  int day = 0;
+  int hour = 0;
+  int minute = 0;
+  int second = 0;
+
+  if (strncmp(serialBuf, "cycle ", 6) == 0) {
+    int n = sscanf(serialBuf + 6, "{%d, %d, %d, %d}", &day, &hour, &minute, &second);
+    if (n == 4) { //success
+      configData = {day, hour, minute, second};
+      saveConfig(CYCLE, c);
+      sprintf(msg, "New cycle config: {%d, %d, %d, %d}", day, hour, minute, second);
+      Serial.println(msg);
+    }
+  }
+
+  if (strncmp(serialBuf, "init ", 5) == 0) {
+    int n = sscanf(serialBuf + 5, "{%d, %d, %d, %d}", &day, &hour, &minute, &second);
+    if (n == 4) { //success
+      configData = {day, hour, minute, second};
+      saveConfig(INIT, c);
+      sprintf(msg, "New init config: {%d, %d, %d, %d}", day, hour, minute, second);
+      Serial.println(msg);
+    }
+  }
+
+  if (strncmp(serialBuf, "get", 3) == 0) {
+    *c = getConfig(INIT);
+    sprintf(msg, "Init config: {%d, %d, %d, %d}", c->day, c->hour, c->minute, c->second);
+    Serial.println(msg);
+    memset(msg, 0, 64);
+    *c = getConfig(CYCLE);
+    sprintf(msg, "Cycle config: {%d, %d, %d, %d}", c->day, c->hour, c->minute, c->second);
+    Serial.println(msg);
+  }
+}
+
+void read_eeprom(char *dist, int addr, int len)
+{
+  memset (dist, 0, len);
+  for (int i = 0; i < len; i++)
+  {
+    dist[i] = EEPROM.read(i + addr);
+  }
+}
+
+void write_eeprom(const char *src, int addr, int len)
+{
+  for (int i = 0; i < len; i++)
+  {
+    char c = *(src + i);
+    EEPROM.write(addr + i, c);
+  }
+}
+
+time_t getConfig(config_type_t type)
+{
+  time_t configData = {0};
+  switch (type) {
+    case INIT:
+      read_eeprom((char*)&configData, INIT_ADDR, INIT_LEN);
+      break;
+    case CYCLE:
+      read_eeprom((char*)&configData, CONFIG_ADDR, CONFIG_LEN);
+      break;
+    default:
+      break;
+  }
+  return configData;
+}
+
+void saveConfig(config_type_t type, time_t *configData)
+{
+  switch (type) {
+    case INIT:
+      write_eeprom((char *)configData, INIT_ADDR, INIT_LEN);
+      break;
+    case CYCLE:
+      write_eeprom((char *)configData, CONFIG_ADDR, CONFIG_LEN);
+      break;
+    default:
+      break;
+  }
+}
+
+void doCountDown()
+{
+  time_t *c = &countdown;
+  char buf[64] = {0};
+  sprintf(buf, "TTR: %d D %d:%d:%d", c->day, c->hour, c->minute, c->second);
+  Serial.println(buf);
+
+  if (countdown.second > 0) {
+    countdown.second --;
+  } else if (countdown.minute > 0) {
+    countdown.minute --;
+    countdown.second = 59;
+  } else if (countdown.hour > 0) {
+    countdown.hour --;
+    countdown.minute = 59;
+    countdown.second = 59;
+  } else if (countdown.day > 0) {
+    countdown.day --;
+    countdown.hour = 23;
+    countdown.minute = 59;
+    countdown.second = 59;
+  } else {
+    //count down is completed, do poweroff action for the consumer
+
+    //flash led to indicate action
+    ledEvent = t.oscillate(ledPin, 50, HIGH);
+
+    switchOff();
+
+    //read hardware setup to determine cycle here:
+    readConfig();
+
+    //slow down led to indicate normal operation after 5 seconds and switch On
+    t.after(switch_seconds * 1000, slowDownLed_switchOn);
+
+  }
+}
+
+void slowDownLed_switchOn()
+{
+  t.stop(ledEvent);
+  t.oscillate(ledPin, 500, HIGH);
+
+  switchOn();
+}
+
+void switchOff()
+{
+  Serial.println("switch Off!!");
+  digitalWrite(powerPin, LOW);
+}
+
+void switchOn()
+{
+  Serial.println("switch On!");
+  digitalWrite(powerPin, HIGH);
+}
+
+bool is_quick_config(int configuration)
+{
+  Serial.print("Config Pin Setup: (b)");
+  Serial.println(configuration, BIN);
+  // this routine check whether there is more
+  // than 1 'true' bits there in the configuration
+  // return false if more than 1 'true' bits there
+  bool found = false;
+  for (int i = 0; i < num_config_bits; i++) {
+    if ((configuration >> i)  & 0x1) {
+      if (found) return false;
+      found = true;
+    }
+  }
+
+  return true;
+}
+
+void readConfig()
+{
+  configuration = 0;
+
+  if (digitalRead(bit1_pin)) configuration |= 0x1;
+  if (digitalRead(bit2_pin)) configuration |= 0x1 << 1;
+  if (digitalRead(bit3_pin)) configuration |= 0x1 << 2;
+  if (digitalRead(bit4_pin)) configuration |= 0x1 << 3;
+  if (digitalRead(bit5_pin)) configuration |= 0x1 << 4;
+  if (digitalRead(bit6_pin)) configuration |= 0x1 << 5;
+
+  if (is_quick_config(configuration)) {
+    //quick config:
+    if (configuration == 0) { //no hard-wired config is done, so use the config in eeprom
+      countdown = getConfig(CYCLE);
+    } else if (configuration & 0x1) {
+      countdown = {0, 1, 0, 0};
+    } else if (configuration & 0x1 << 1) {
+      countdown = {0, 2, 0, 0};
+    } else if (configuration & 0x1 << 2) {
+      countdown = {0, 4, 0, 0};
+    } else if (configuration & 0x1 << 3) {
+      countdown = {0, 6, 0, 0};
+    } else if (configuration & 0x1 << 4) {
+      countdown = {0, 12, 0, 0};
+    } else if (configuration & 0x1 << 5) {
+      countdown = {2, 0, 0, 0};
+    }
+  }
+  else {
+    //if it is not quick config, one bit equals to 30 minutes
+    countdown.day    = (configuration / 24) / 2;
+    countdown.hour   = (configuration / 2) % 24;
+    countdown.minute = (configuration % 2) * 30;
+    countdown.second = 0;
+  }
+}
+
+
+
